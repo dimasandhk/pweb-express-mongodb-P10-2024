a961ebd37521071f6f68b7d8136c7ce32e3f00d8
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-11-22 15:16:58 +0700

    feat: adjustment for pweb modul 4

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -4,11 +4,18 @@ const router = express.Router();
 
 import { auth } from "../middleware/auth";
 
-router.get("/books", auth, BookController.getAll);
-router.post("/book", auth, BookController.create);
+// router.get("/books", auth, BookController.getAll);
+// router.post("/book", auth, BookController.create);
 
-router.get("/book/:id", auth, BookController.getById);
-router.patch("/book/:id", auth, BookController.update);
-router.delete("/book/:id", auth, BookController.delete);
+// router.get("/book/:id", auth, BookController.getById);
+// router.patch("/book/:id", auth, BookController.update);
+// router.delete("/book/:id", auth, BookController.delete);
+
+router.get("/books", BookController.getAll);
+router.post("/book", BookController.create);
+
+router.get("/book/:id", BookController.getById);
+router.patch("/book/:id", BookController.update);
+router.delete("/book/:id", BookController.delete);
 
 export default router;
